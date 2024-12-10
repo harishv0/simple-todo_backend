5b30275e9e09fc507179cd7fd74b765bc87f8abc
harishv0 <harishv2505@gmail.com>
2024-12-10 10:53:13 +0530

    first commit

--- src/main/java/com/example/backendd/TaskController.java
+++ src/main/java/com/example/backendd/TaskController.java
@@ -10,15 +10,14 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/task")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://simplet-todo.netlify.app")
 public class TaskController {
 
     @Autowired
