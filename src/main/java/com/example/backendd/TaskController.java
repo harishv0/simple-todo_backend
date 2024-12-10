e51638b45548df14955e58d4540146fa189f4982
harishv0 <harishv2505@gmail.com>
2024-12-10 23:34:56 +0530

    V1

--- src/main/java/com/example/backendd/TaskController.java
+++ src/main/java/com/example/backendd/TaskController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/task")
-@CrossOrigin(origins = "https://simplet-todo.netlify.app")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TaskController {
 
     @Autowired
